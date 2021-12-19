0f0ac46d4a58ce0c09a9c1af1faa35dc2b8cfc8c
anon <anon>
2021-12-20 02:04:09 +0300

    final

--- client.py
+++ client.py
@@ -1,14 +1,35 @@
 import socket
-import pickle
+from protocol import Diffie_Hellman_Protocol
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8082
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
+print(f"Подсоединились к порту: {PORT}")
+#g-client_public_key
+#p-server_public_key
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+crypt_client = Diffie_Hellman_Protocol()
+crypt_client.bunch_of_public_keys()
+keys = str(crypt_client.client_public_key)+' '+str(crypt_client.server_public_key)
+sock.send(keys.encode())
 
-sock.close()
+
+msg = sock.recv(1024).decode()
+if msg == "Доступ разрешен":
+    print(msg+"\nTo exit, send \"exit\"")
+    server_key_partial = int(sock.recv(1024).decode())
+    client_partial_key = crypt_client.generate_partial_key()
+    sock.send(str(client_partial_key).encode())  # отправляем частичный ключ клиента (А) серверу
+    crypt_client.generate_full_key(server_key_partial)
+    while True:
+        msg = input(""">>""")
+        if msg == 'exit' or msg == 'Exit':
+            sock.send(crypt_client.encrypt_message(msg).encode())
+            break
+        sock.send(crypt_client.encrypt_message(msg).encode())
+    sock.close()
+else:
+    print("Доступ запрещен")
+    sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,45 @@
+import random
 import socket
-import pickle
+from protocol import Diffie_Hellman_Protocol
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8082
 
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
+print(f'Слушаем {PORT} порт...')
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+def make_keys(conn):
+    bunch = conn.recv(2054).decode()
+    bunch = bunch.split(' ')
+    crypt_server = Diffie_Hellman_Protocol(int(bunch[0]), int(bunch[1]), random.randint(1, 320))
+    return crypt_server
 
-conn.close()
+def check_access(client_public_key):
+    with open('public_keys.txt', 'r') as file:
+        flag = False
+        for line in file:
+            if int(line) == client_public_key:
+                flag = True
+                break
+    return flag
+
+crypt_server = make_keys(conn)
+
+if check_access(crypt_server.client_public_key):
+    conn.send("Доступ разрешен".encode())
+    server_partial_key = crypt_server.generate_partial_key()
+    conn.send(str(server_partial_key).encode())
+    client_key_partial = int(conn.recv(1024).decode())
+    print(client_key_partial)
+    crypt_server.generate_full_key(client_key_partial)
+    while True:
+        msg = conn.recv(2024).decode()
+        print(f'Зашированное сообщение: {msg} \nЗашифрованное сообщение: {crypt_server.decrypt_message(msg)}\n')
+        if crypt_server.decrypt_message(msg) == 'Exit' or crypt_server.decrypt_message(msg) == 'exit':
+            break
+    conn.close()
+else:
+    conn.close()
